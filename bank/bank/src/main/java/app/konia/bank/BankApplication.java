967903a946855bc27219e89f7de254503fc0740e
carlos dominciano <carlos.dominciano@bandtec.com.br>
2023-03-29 23:48:10 -0300

    feat: CRUD created with h2-console conection

--- bank/bank/src/main/java/app/konia/bank/BankApplication.java
+++ bank/bank/src/main/java/app/konia/bank/BankApplication.java
@@ -8,5 +8,28 @@ public class BankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
-
 }
+//3 - Desafio Java Spring Boot e React
+//
+//		Desenvolva uma API Rest em Spring Boot para operações bancárias.
+//		A API deverá ter as seguintes funcionalidades:
+//		- Criar boleto
+//		- Listar boletos
+//		- Ver detalhes dos boletos
+//		- Pagar um boleto
+//		- Cancelar um boleto
+//
+//		Para o desenvolvimento, utilize um banco de dados PostgreSQL com Docker.
+//
+//		Desenvolva um projeto React, que realize as operações bancárias consultando
+//		os endpoints da API. A interface do usuário não precisa ser muito estilizada,
+//		apenas responsiva e funcional.
+//
+//		Critérios de avaliação:
+//		- Aplicação de padrões de código
+//		- Tratamento de erros ao usuário
+//		- Tratamento de erros em backend
+//		- Usabilidade da interface
+//
+//Critérios obrigatórios:
+//		- Criação de documentação técnica para execução do projeto (README)
